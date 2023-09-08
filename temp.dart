67accf47ec15034f17d248de3a9029f9994c5e0e
Fahim1240 <emranhossainfahim15@gmail.com>
2023-09-08 19:10:45 +0600

    Third Commit

--- temp.dart
+++ temp.dart
@@ -9,3 +9,27 @@ void main() {
       'Choose conversion (1 for Celsius to Fahrenheit, 2 for Fahrenheit to Celsius): ');
 
   int choice = int.parse(stdin.readLineSync()!);
+
+  double result;
+
+  String unitFrom, unitTo;
+  if (choice == 1) {
+    result = (temperature * 9 / 5) + 32;
+
+    unitFrom = 'Celsius';
+
+    unitTo = 'Fahrenheit';
+  } else if (choice == 2) {
+    result = (temperature - 32) * 5 / 9;
+
+    unitFrom = 'Fahrenheit';
+
+    unitTo = 'Celsius';
+  } else {
+    print('Invalid choice. Please choose 1 or 2 for conversion.');
+
+    return;
+  }
+
+  print('$temperature $unitFrom is equal to $result $unitTo.');
+}
